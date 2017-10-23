e3868c09c66be8b15901219ddf9d4d188a7669c3
jaredburks <jarabu93@gmail.com>
2017-10-23 13:16:12 -0500

    Deleted role controller from models folder and moved to controlers folder.

--- VendingMachine/Controllers/RoleController.cs
+++ VendingMachine/Controllers/RoleController.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VendingMachine.Models;
 
-namespace VendingMachine.Models
+namespace VendingMachine.Controllers
 {
     public class RoleController : Controller
     {
